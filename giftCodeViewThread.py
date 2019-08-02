fa52d33f080171d927a41f083bcc464af4da5aff
Manito <mantao@acxiom.com>
2019-08-02 12:33:58 +0800

    update thread logic

--- giftCodeViewThread.py
+++ giftCodeViewThread.py
@@ -4,55 +4,41 @@
 import requests
 import sys
 import threading
+from uitls import chunks
 
-
-def job(uid, flag, code):
+def job(uids, code):
     """获取指定ip对应的地理位置"""
-    if not flag:
-        pass
-    url='http://statistics.pandadastudio.com/player/giftCode'
-    infoUrl = 'http://statistics.pandadastudio.com/player/simpleInfo?uid=203895417'
-    giftCodeReq = requests.get(url, params={"uid": uid, "code": code})
-    giftCodeRes=giftCodeReq.json()
-    
-
-
-    infoReq = requests.get(infoUrl, params={"uid": uid})
-    infoRes=infoReq.json()
-    if infoRes['msg'] == 'success':
-        print(uid, giftCodeRes['msg'], infoRes['data']['name'])
-    else:
-        print(uid, giftCodeRes['msg'])
-
-    # # 417: 礼包不存在 424: 过期
-    # if result['code'] == 417 or result['code'] == 424:
-    #     print(result['msg'])
-    #     flag=False
-    #     pass
-    # else:
-    #     print(uid, result['msg'])
-
-
-
+    flag=True
+    for uid in uids:
+        if not flag:
+            break
+        url='http://statistics.pandadastudio.com/player/giftCode'
+        infoUrl = 'http://statistics.pandadastudio.com/player/simpleInfo?uid=203895417'
+        giftCodeReq = requests.get(url, params={"uid": uid, "code": code})
+        giftCodeRes=giftCodeReq.json()
+        # # 417: 礼包不存在 424: 过期
+        if giftCodeRes['code'] == 417 or giftCodeRes['code'] == 424:
+            print(giftCodeRes['msg'])
+            flag = False
+            break
+        else:
+            # 获取基本信息
+            infoReq = requests.get(infoUrl, params={"uid": uid})
+            infoRes=infoReq.json()
+            if infoRes['msg'] == 'success':
+                print(uid, giftCodeRes['msg'], infoRes['data']['name'])
+            else:
+                print(uid, giftCodeRes['msg'])
 
 if __name__ == "__main__":
     threads = []
     uids=[]
-    flag=True
     code=sys.argv[1]
     print('兑换码=>' + code)
     with open('uids.txt', 'r') as f:
-        # while True:
-        #     line = file.readline() # 整行读取数据
-        #     if not line:
-        #         break
-        #         pass
-        #     print(line)
-        #     uids.append(line)
         uids = f.read().splitlines()
-    
-    for uid in uids:
-        t = threading.Thread(target=job, args=(uid, flag, code))
+    for _uids in chunks(uids, 5):
+        t = threading.Thread(target=job, args=(_uids, code))
         threads.append(t)
         t.start()
 
